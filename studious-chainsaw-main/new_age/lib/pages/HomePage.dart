8f2602258d24c916330f7618797a1990f880ce48
samwin <samwinpereira@gmail.com>
2023-04-25 18:51:52 +0530

    home page

--- studious-chainsaw-main/new_age/lib/pages/HomePage.dart
+++ studious-chainsaw-main/new_age/lib/pages/HomePage.dart
@@ -5,53 +5,118 @@ import 'package:new_age/pages/maptest_page.dart';
 import 'package:new_age/pages/mil_info.dart';
 import 'package:new_age/pages/profile_page.dart';
 import '../widgets/widgets.dart';
+import 'package:lottie/lottie.dart';
 
 
 
-
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-
-
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-            const Padding(
-              padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
-              child: Text(
-                "Welcome, To WiseWind",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 27, 94, 32),
-                  fontSize: 50,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12, 12, 12, 8),
+              child: Card(
+                color: Color(0xFF307A27),
+                elevation: 4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    gradient: LinearGradient(
+                      end: Alignment.topLeft,
+                      begin: Alignment(0.8, 1),
+                      colors: <Color>[
+                        Color(0xff19906A),
+                        Color(0xff158368),
+                        Color(0xff0D7066),
+                        Color(0xff015A62),
+                      ], // Gradient from https://learnui.design/tools/gradient-generator.html
+                      tileMode: TileMode.mirror,
+                    ),
+                  ),
+                  child: InkWell(
+                    onTap: () {
+                      // Add your navigation logic here
+                    },
+                    child: Container(
+                      height: 200,
+                      padding: const EdgeInsets.all(26),
+                      width: double.infinity,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  'Welcome Joywin!!',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontFamily: 'poppins',
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Wind energy accounts for 6.59% of the power generated worldwide!',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                    fontFamily: 'poppins',
+                                  ),
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: 100,
+                            height: 100,
+                            child: Lottie.asset("assets/windmill-icon.json"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 16,),
+            Container(
+              height: 80,
+              width: 200,
+              decoration: BoxDecoration(
+                color: Colors.orangeAccent,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Lets Analyse'),
+                    const SizedBox(width: 10),
+                    const Icon(Icons.arrow_forward),
+                  ],
                 ),
               ),
             ),
             const SizedBox(
-              height: 15,
-            ),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "     Wind Mills",
-                style: TextStyle(
-                    color: Color.fromARGB(255, 102, 187, 106),
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
+              height: 16,
             ),
             Container(
               height: 350,
@@ -59,15 +124,19 @@ class _HomePageState extends State<HomePage> {
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children:  [
-                   MillInfo(millname: "Vertical mill",millinfo: "this is the info page\nwhich has basic info\ngod just kill me" , imagePath: "assets/vaxi.gif", page: ProfilePage()),
-                   MillInfo(millname: "Horizontal", millinfo: "this is the info page\nwhich has basic info\ngod just kill me", imagePath: "assets/vaxi.gif", page: ProfilePage()),
-                   MillInfo(millname: "Horizontal", millinfo: "this is the info page\nwhich has basic info\ngod just kill me", imagePath: "assets/vaxi.gif", page: ProfilePage()),                 
-                   
+                    MillInfo(millname: "Vertical mill",millinfo: "this is the info page\nwhich has basic info\ngod just kill me" , imagePath: "assets/vaxi.gif", page: ProfilePage()),
+                    MillInfo(millname: "Vertical mill",millinfo: "this is the info page\nwhich has basic info\ngod just kill me" , imagePath: "assets/vaxi.gif", page: ProfilePage()),
+                    MillInfo(millname: "Vertical mill",millinfo: "this is the info page\nwhich has basic info\ngod just kill me" , imagePath: "assets/vaxi.gif", page: ProfilePage()),
+
+
+            
                   ],
                 ),
               ),
             ),
           ],
-        );
+        ),
+      ),
+    );
   }
 }
